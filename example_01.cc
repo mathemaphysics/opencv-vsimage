89a76814f26e4141d4dac050b98b188bbbbd2dc4
Ryan P. Daly <rdaly@herzog.com>
2019-12-12 17:27:25 +0000

    Have a working example_01.cc for testing functions

--- example_01.cc
+++ example_01.cc
@@ -8,16 +8,32 @@
 #include <algorithm>
 #include <vector>
 
-int iterator_function(int input)
+int iterator_function(int &input)
 {
-    return 2 * input;
+    input = 2 * input;
 }
 
 int main(int argc, char **argv)
 {
+    /**
+     * Introduce yourself
+     */
     std::cout << "Hello, World!" <<std::endl;
     std::vector<int> vec {{ 1, 2, 3, 4, 5, 6, 7 }};
+
+    /* Before std::for_each */
+    std::cout << "After:" << std::endl;
+    for (auto itr = vec.begin(); itr < vec.end(); itr++)
+        std::cout << *itr << std::endl;
+
+    /* Run std::for_each */
     std::for_each(vec.begin(), vec.end(), &iterator_function);
 
+    /* After std::for_each */
+    std::cout << "After:" << std::endl;
+    for (auto itr = vec.begin(); itr < vec.end(); itr++)
+        std::cout << *itr << std::endl;
+
+    /* Something, something, something... Daaaark Siiide */
     return 0;
 }
