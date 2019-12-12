0c12618f0f1d4bea3a4b7816e2894d1dacba1752
Ryan P. Daly <rdaly@herzog.com>
2019-12-12 16:37:21 +0000

    Configuration changes mainly

--- example_01.cc
+++ example_01.cc
@@ -4,10 +4,20 @@
  * code.
  */
 
-
 #include <iostream>
+#include <algorithm>
+#include <vector>
+
+int iterator_function(int input)
+{
+    return 2 * input;
+}
 
 int main(int argc, char **argv)
 {
-    
+    std::cout << "Hello, World!" <<std::endl;
+    std::vector<int> vec {{ 1, 2, 3, 4, 5, 6, 7 }};
+    std::for_each(vec.begin(), vec.end(), &iterator_function);
+
+    return 0;
 }
